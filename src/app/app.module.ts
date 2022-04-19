9401199438b9b50033acd79abfef013b1b35d0b9
Le-Tuan-2704 <tuan.ld184004@sis.hust.edu.vn>
2022-04-19 20:31:37 +0700

    khung html trang chính

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SiderComponent } from './layout/sider/sider.component';
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    SiderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
